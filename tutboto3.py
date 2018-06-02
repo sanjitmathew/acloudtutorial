00b4ec917bbdc0e3838fd500ac1c3f419ddcff72
sanjitmathew <sanjitmathewranjit@gmail.com>
2018-06-02 10:35:43 +0530

    boto3 program

---
+++ tutboto3.py
@@ -0,0 +1,14 @@
+#configure aws in and let instance be 'shotty'
+import boto3
+
+if __name__=='__main__':    #doesn't work when imported into another module 
+    #creates a session on the aws instance shotty
+    session=boto3.Session(profile_name='shotty')
+
+    #get the resource for each type of service
+    ec2=session.resource('ec2')
+
+    #add permissions to users in aws, manage policy
+
+    for i in ec2.instances.all():
+        print i
